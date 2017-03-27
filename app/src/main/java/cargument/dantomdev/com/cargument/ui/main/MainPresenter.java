3d1e97cb71feef5757231d3f285aa2640a89af8f
Deák Tamás <tamas.deak.93@gmail.com>
2017-03-27 10:08:54 +0200

    MVP implemented in MainScreen

---
+++ app/src/main/java/cargument/dantomdev/com/cargument/ui/main/MainPresenter.java
@@ -0,0 +1,28 @@
+package cargument.dantomdev.com.cargument.ui.main;
+
+import cargument.dantomdev.com.cargument.ui.Presenter;
+
+public class MainPresenter extends Presenter<MainScreen> {
+
+    private static MainPresenter instance = null;
+
+    private MainPresenter() {
+    }
+
+    public static MainPresenter getInstance() {
+        if (instance == null) {
+            instance = new MainPresenter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+}
---
+++ app/src/main/java/cargument/dantomdev/com/cargument/ui/main/MainScreen.java
@@ -0,0 +1,5 @@
+package cargument.dantomdev.com.cargument.ui.main;
+
+public interface MainScreen {
+    void showMessage(String text);
+}
